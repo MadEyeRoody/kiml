2290ed32a67df4df988d8e7255e27a132588a277
MadEyeRoody <rudi.mottinger@gmail.com>
2016-03-02 09:56:16 +0100

    ionic creater sample added

--- camelo/www/js/controllers.js
+++ camelo/www/js/controllers.js
@@ -1,38 +1,52 @@
 angular.module('app.controllers', [])
-  
-.controller('kIMLKannIchsMirLeistenCtrl', function($scope) {
 
+.controller('kIMLKannIchsMirLeistenCtrl', function($scope, $state) {
+$scope.checkAmount = function(betrag){
+
+  betragValue=parseInt(betrag);
+
+  console.log(betragValue);
+
+  if(betragValue > 500){
+    $state.go('menu.empfehlung2');
+    window.localStorage.setItem("amount", betragValue)
+  } else {
+    $state.go('menu.empfehlung');
+  }
+
+  };
 })
-   
+
 .controller('wunschlisteCtrl', function($scope) {
 
 })
-   
+
 .controller('finanzstatusCtrl', function($scope) {
 
 })
-      
+
 .controller('empfehlungCtrl', function($scope) {
 
 })
-   
+
 .controller('empfehlung2Ctrl', function($scope) {
 
+  $scope.amount = window.localStorage.getItem("amount");
+
 })
-   
+
 .controller('kontoverwaltungCtrl', function($scope) {
 
 })
-   
+
 .controller('umbuchungCtrl', function($scope) {
 
 })
-   
+
 .controller('kreditCtrl', function($scope) {
 
 })
-   
+
 .controller('kredit2Ctrl', function($scope) {
 
 })
- 
