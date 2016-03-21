6551d0b29184ffa168aa4327a99508121637cfb0
Ferdinand Schneider <ferdinand.schneider@shks.de>
2016-03-21 14:25:49 +0100

    FS:impl del feature in wishlist

--- camelo/www/js/controllers/wunschliste.js
+++ camelo/www/js/controllers/wunschliste.js
@@ -1,8 +1,13 @@
-angular.module('app.controllers').controller(
-    'wunschlisteCtrl',
+angular.module('app.controllers').controller('wunschlisteCtrl',
     function($scope, $state, wishlist) {
       $scope.wunschliste = wishlist.list;
-      
+
+      $scope.edit = false;
+
+      $scope.swapEdit = function() {
+        $scope.edit = !$scope.edit;
+      }
+
       $scope.onAdd = function() {
         $state.go('menu.addWunsch');
       }
