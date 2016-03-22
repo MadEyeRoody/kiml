cd5d8127f2b4cdf5fa1ddb2a7004fc2cf09705c1
Ferdinand Schneider <ferdinand.schneider@shks.de>
2016-03-22 13:24:10 +0100

    FS: enhance edit mode

--- camelo/www/js/controllers/wunschliste.js
+++ camelo/www/js/controllers/wunschliste.js
@@ -6,9 +6,19 @@ angular.module('app.controllers').controller('wunschlisteCtrl',
 
       $scope.swapEdit = function() {
         $scope.edit = !$scope.edit;
-      }
-
+      };
+      
+      $scope.moveItem = function(item, fromIndex, toIndex) {
+        //Move the item in the array
+        $scope.wunschliste.splice(fromIndex, 1);
+        $scope.wunschliste.splice(toIndex, 0, item);
+      };
+      
       $scope.onAdd = function() {
         $state.go('menu.addWunsch');
-      }
+      };
+
+      $scope.onItemClicked = function(item) {
+        alert(item.name);
+      };
     });
