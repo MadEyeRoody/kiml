4834cba79f535069717e6f656df36a945a074dec
MadEyeRoody <rudi.mottinger@gmail.com>
2016-03-23 14:11:10 +0100

    style changes

--- camelo/www/js/controllers/kIMLKannIchsMirLeisten.js
+++ camelo/www/js/controllers/kIMLKannIchsMirLeisten.js
@@ -33,6 +33,7 @@ angular
                   console.log("Scan cancelled");
                 } else {
                   getPreisToBarcode(result.format, result.text);
+                  $scope.openModal(result.format, result.text);
                 }
               }, function(error) {
                 // An error occurred
@@ -61,4 +62,37 @@ angular
               });
             }
           }
+
+          $ionicModal.fromTemplateUrl('modalScan.html', {
+            scope : $scope,
+
+            animation : 'slide-in-up',
+
+          }).then(function(modal) {
+            $scope.modal = modal;
+            console.log(modal);
+          });
+          $scope.openModal = function(format, data) {
+            $scope.format = format;
+            $scope.data = data;
+            $scope.modal.show();
+          };
+          $scope.closeModal = function() {
+
+            $scope.modal.hide();
+          };
+          // Cleanup the modal when we're done with it!
+          $scope.$on('$destroy', function() {
+
+            $scope.modal.remove();
+          });
+          // Execute action on hide modal
+          $scope.$on('modal.hidden', function() {
+            // Execute action
+          });
+          // Execute action on remove modal
+          $scope.$on('modal.removed', function() {
+            // Execute action
+          });
+
         });
